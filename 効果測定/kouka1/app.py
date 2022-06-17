70556b486cc726943d4db10ab15a4fede081df2c
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-06-17 23:52:29 +0900

    kouka1　体裁を修正　感情分析機能の追加

--- 効果測定/kouka1/app.py
+++ 効果測定/kouka1/app.py
@@ -10,19 +10,18 @@ import sys
 # インスタンスの作成
 app = Flask(__name__)
 
-# メイン
-@app.route('/', methods=['GET', 'POST'])
 # アクセスした時に実行する関数を直下に記述する
+# -----------translate(翻訳)--------------
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # methodが POST かを判断
     if request.method == 'POST':
         # Translate サービスクライアントを作成
         translate = boto3.client('translate')
         # 翻訳前テキストを取得
-        getText = request.form.get('texts')
+        getText = request.form.get('pretext')
         preText = getText
         # 翻訳言語の取得
-        language = 'ja' #デフォルトは日本語
         language = request.form.get('language')
         SourceLanguage = language
         # 翻訳
@@ -40,6 +39,33 @@ def index():
     return render_template('index.html',
                 transText = transText,
                 preText = preText)
+
+# -----------conprehend(感情分析)--------------
+@app.route('/comprehend', methods=['POST'])
+def comprehend():
+        # Comprehend サービスクライアントを作成
+        comprehend = boto3.client('comprehend', 'us-east-1')
+        # 感情分析するテキストを取得
+        getText = request.form.get('pretext')
+        text = getText
+        # 感情分析するテキストの言語を取得
+        result = comprehend.detect_dominant_language(Text=text)
+        language = result['Languages'][0]['LanguageCode'] #Languagesには複数の言語コードがあり得る
+        # 感情を分析
+        result = comprehend.detect_sentiment(Text=text, LanguageCode=language)
+        # 結果を表示
+        sentiment = result['Sentiment']  #推測した感情
+        positive = result['SentimentScore']['Positive'] #肯定的な感情のスコア
+        negative = result['SentimentScore']['Negative'] #否定的な感情のスコア
+        neutral = result['SentimentScore']['Neutral'] #中立的な感情のスコア
+        mixed = result['SentimentScore']['Mixed'] #混じった感情のスコア
+        return render_template('comprehend.html',
+                        sentiment = sentiment,
+                        language = language,
+                        positive = round((positive * 100),2),
+                        negative = round((negative * 100),2),
+                        neutral = round((neutral * 100),2),
+                        mixed = round((mixed * 100),2))
 # アプリケーションの起動 おまじない debug=Trueでエラーを表示してくれる
 if __name__ == '__main__':
     app.run(debug=True)
